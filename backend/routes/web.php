4c914483898e309171918cad372da274ff45b43b
tanayu567 <tana-yu.567@outlook.jp>
2021-10-05 15:56:45 +0900

    タスク一覧のルーティング

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,17 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    logger('welcome route.');
-    return view('welcome');
-});
+Route::get('folders/{id}/tasks', 'TasksController@index') -> name('tasks.index');
