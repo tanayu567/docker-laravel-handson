69df59105c61250c8acc0491d20521b236841311
tanayu567 <tana-yu.567@outlook.jp>
2021-10-06 19:10:18 +0900

    FolderとTaskの紐付け

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -14,7 +14,7 @@ class TaskController extends Controller
 
         $current_folder = Folder::find($id);
 
-        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
