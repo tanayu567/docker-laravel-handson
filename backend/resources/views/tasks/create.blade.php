fe56119cec5c766ab393a7059d6f8e413354de82
tanayu567 <tana-yu.567@outlook.jp>
2021-10-07 11:42:34 +0900

    レイアウトでテンプレートと整理

--- backend/resources/views/tasks/create.blade.php
+++ backend/resources/views/tasks/create.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ToDo App</title>
+@extends('layout')
+
+@section('styles')
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
   <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-<header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
-  </nav>
-</header>
-<main>
+@endsection
+  
+@section('content')
   <div class="container">
     <div class="row">
       <div class="col col-md-offset-3 col-md-6">
@@ -48,8 +38,9 @@
       </div>
     </div>
   </div>
-</main>
+@endsection
 
+@section('scripts')
 <script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
 <script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
 <script>
@@ -59,5 +50,4 @@
     minDate: new Date()
   });
 </script>
-</body>
-</html>
+@endsection
