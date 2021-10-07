d7d18f2710f9aed047ad739d377efeeea6203a1e
tanayu567 <tana-yu.567@outlook.jp>
2021-10-07 12:19:17 +0900

    タスク作成機能追加

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Folder;
 use App\Task;
+use App\Http\Requests\CreateTask;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -27,6 +28,21 @@ class TaskController extends Controller
     {
         return view('tasks/create', [
             'folder_id' =>$id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
