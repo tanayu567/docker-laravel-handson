55132bcede99e9fa0f3d0093b824a599730866b2
tanayu567 <tana-yu.567@outlook.jp>
2021-10-08 14:11:47 +0900

    メッセージの日本語化

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -8,3 +8,4 @@ Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name(
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+Auth::routes();
